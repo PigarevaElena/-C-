f5d52f958974485dbcfb9305649a4c9b28380b23
Pigareva Elena <magdalenka-@mail.ru>
2023-08-11 14:35:27 +0300

    Определение четного числа

--- Program.cs
+++ Program.cs
@@ -39,3 +39,14 @@ Console.WriteLine(max);*/
 
 /*Напишите программу, которая на вход принимает число и выдаёт, 
 является ли число чётным (делится ли оно на два без остатка)*/
+
+/*Console.Write("Введите число: ");
+int number = int.Parse (Console.ReadLine());
+if(number %2==0)
+{
+    Console.WriteLine($"Да");
+}
+else 
+{
+    Console.WriteLine($"Нет");
+}*/
